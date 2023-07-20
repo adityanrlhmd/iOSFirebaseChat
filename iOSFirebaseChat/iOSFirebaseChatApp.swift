b7efbc6ba3a72ab7532f4352085596e0c578ed44
adityanrlhmd <adityanurulhamda@gmail.com>
2023-07-20 19:58:39 +0700

    Initial Commit

---
+++ iOSFirebaseChat/iOSFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFirebaseChatApp.swift
+//  iOSFirebaseChat
+//
+//  Created by Aditt on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
